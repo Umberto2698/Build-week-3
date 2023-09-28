a7712f466009223e21433c82d4368b9458de05da
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-28 13:45:52 +0200

    minor fix button to profile

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
-import { Button, Col, Container, Dropdown, Form, InputGroup, Navbar, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Dropdown,
+  Form,
+  InputGroup,
+  Navbar,
+  Row,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -51,10 +60,19 @@ const MyNavbar = () => {
   return (
     user && (
       <>
-        <Navbar fixed="top" expand="lg" style={{ height: "53px", zIndex: "10" }} className="bg-white p-0">
+        <Navbar
+          fixed="top"
+          expand="lg"
+          style={{ height: "53px", zIndex: "10" }}
+          className="bg-white p-0"
+        >
           <Container fluid="xl" style={{ height: "100%" }}>
             <Row className="flex-grow-1 " style={{ height: "100%" }}>
-              <Col xs={1} lg={4} className="p-1  align-items-center d-none d-md-flex">
+              <Col
+                xs={1}
+                lg={4}
+                className="p-1  align-items-center d-none d-md-flex"
+              >
                 <Link to="/" className="me-2">
                   <img
                     src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
@@ -64,7 +82,11 @@ const MyNavbar = () => {
                   />
                 </Link>
 
-                <Form onSubmit={handleSubmit} style={{ maxWidth: "280px" }} className="flex-grow-1 d-none d-lg-block ">
+                <Form
+                  onSubmit={handleSubmit}
+                  style={{ maxWidth: "280px" }}
+                  className="flex-grow-1 d-none d-lg-block "
+                >
                   <InputGroup>
                     <InputGroup.Text
                       id="basic-addon1"
@@ -94,11 +116,20 @@ const MyNavbar = () => {
                 }}
               >
                 <Link to="/profile" className="m-auto">
-                  <img src={user.image} width={32} height={32} className="rounded-circle " alt="user" />
+                  <img
+                    src={user.image}
+                    width={32}
+                    height={32}
+                    className="rounded-circle "
+                    alt="user"
+                  />
                 </Link>
               </Col>
               <Col className="py-2 px-0 d-flex d-md-none ">
-                <Form onSubmit={handleSubmit} className="d-flex flex-grow-1 justify-content-center">
+                <Form
+                  onSubmit={handleSubmit}
+                  className="d-flex flex-grow-1 justify-content-center"
+                >
                   <InputGroup>
                     <InputGroup.Text
                       id="basic-addon1"
@@ -124,7 +155,10 @@ const MyNavbar = () => {
                   className="nav-link d-flex flex-column p-0 align-items-center my-navbar-text justify-content-center "
                   style={{ color: "#8d8d8d", height: "100%" }}
                 >
-                  <FontAwesomeIcon icon={faArrowDownWideShort} style={{ height: "20px" }} />
+                  <FontAwesomeIcon
+                    icon={faArrowDownWideShort}
+                    style={{ height: "20px" }}
+                  />
                 </Link>
               </Col>
               <Col xs={1} className="d-md-none" style={{ height: "100%" }}>
@@ -133,11 +167,18 @@ const MyNavbar = () => {
                   className="nav-link d-flex flex-column p-0 align-items-center my-navbar-text justify-content-center "
                   style={{ color: "#8d8d8d", height: "100%" }}
                 >
-                  <FontAwesomeIcon icon={faCommentDots} style={{ height: "20px" }} />
+                  <FontAwesomeIcon
+                    icon={faCommentDots}
+                    style={{ height: "20px" }}
+                  />
                 </Link>
               </Col>
 
-              <Col xs={10} lg={7} className="align-items-center flex-grow-1 d-none d-md-flex">
+              <Col
+                xs={10}
+                lg={7}
+                className="align-items-center flex-grow-1 d-none d-md-flex"
+              >
                 <Row className="flex-grow-1" style={{ height: "100%" }}>
                   <Col className="d-lg-none" style={{ height: "100%" }}>
                     <Link
@@ -156,7 +197,10 @@ const MyNavbar = () => {
                       }`}
                       style={{ color: "#8d8d8d", height: "100%" }}
                     >
-                      <FontAwesomeIcon icon={faHouse} style={{ height: "20px" }} />
+                      <FontAwesomeIcon
+                        icon={faHouse}
+                        style={{ height: "20px" }}
+                      />
                       <span className="d-none d-md-block"> Home</span>
                     </Link>
                   </Col>
@@ -168,7 +212,10 @@ const MyNavbar = () => {
                       }`}
                       style={{ color: "#8d8d8d", height: "100%" }}
                     >
-                      <FontAwesomeIcon icon={faUserGroup} style={{ height: "20px" }} />
+                      <FontAwesomeIcon
+                        icon={faUserGroup}
+                        style={{ height: "20px" }}
+                      />
                       <span className="d-none d-md-block">Rete</span>
                     </Link>
                   </Col>
@@ -180,7 +227,10 @@ const MyNavbar = () => {
                       }`}
                       style={{ color: "#8d8d8d", height: "100%" }}
                     >
-                      <FontAwesomeIcon icon={faBriefcase} style={{ height: "20px" }} />
+                      <FontAwesomeIcon
+                        icon={faBriefcase}
+                        style={{ height: "20px" }}
+                      />
                       <span className="d-none d-md-block"> Lavoro</span>
                     </Link>
                   </Col>
@@ -192,7 +242,10 @@ const MyNavbar = () => {
                       }`}
                       style={{ color: "#8d8d8d", height: "100%" }}
                     >
-                      <FontAwesomeIcon icon={faCommentDots} style={{ height: "20px" }} />
+                      <FontAwesomeIcon
+                        icon={faCommentDots}
+                        style={{ height: "20px" }}
+                      />
                       <span className="d-none d-md-block"> Messaggistica</span>
                     </Link>
                   </Col>
@@ -204,11 +257,17 @@ const MyNavbar = () => {
                       }`}
                       style={{ color: "#8d8d8d", height: "100%" }}
                     >
-                      <FontAwesomeIcon icon={faBell} style={{ height: "20px" }} />
+                      <FontAwesomeIcon
+                        icon={faBell}
+                        style={{ height: "20px" }}
+                      />
                       <span className="d-none d-md-block">Notifiche</span>
                     </Link>
                   </Col>
-                  <Col className=" d-none d-md-block" style={{ height: "100%", borderRight: "solid 1px #e5e5e5" }}>
+                  <Col
+                    className=" d-none d-md-block"
+                    style={{ height: "100%", borderRight: "solid 1px #e5e5e5" }}
+                  >
                     <Dropdown
                       className="nav-link d-flex flex-column p-0 align-items-center my-navbar-text justify-content-center"
                       style={{ height: "100%" }}
@@ -243,7 +302,10 @@ const MyNavbar = () => {
                         drop={"start"}
                         style={{ backgroundColor: "white" }}
                       >
-                        <div className="text-decoration-none dropdown-item p-2 " style={{ backgroundColor: "white" }}>
+                        <div
+                          className="text-decoration-none dropdown-item p-2 "
+                          style={{ backgroundColor: "white" }}
+                        >
                           <div className="d-flex">
                             <Link to="/profile" className="link">
                               <div className="p-2">
@@ -260,7 +322,11 @@ const MyNavbar = () => {
                               </div>
                             </Link>
                             <div className="flex-grow-1">
-                              <Link to="/profile" className="link" style={{ color: "black" }}>
+                              <Link
+                                to="/profile"
+                                className="link"
+                                style={{ color: "black" }}
+                              >
                                 <h6>
                                   {user.name} {user.surname}
                                 </h6>
@@ -275,7 +341,6 @@ const MyNavbar = () => {
                             className="rounded-pill py-0"
                             size="sm"
                             style={{
-                              backgroundColor: "transparent",
                               color: "#0d6efd",
                               width: "100%",
                             }}
@@ -296,13 +361,18 @@ const MyNavbar = () => {
                         <hr className="m-0" />
                         <h6 className="pt-3 ps-3">Gestisci</h6>
                         <Dropdown.Item>Post e attivitá</Dropdown.Item>
-                        <Dropdown.Item className="text-truncate">Account per la pubblicazione di offerte</Dropdown.Item>
+                        <Dropdown.Item className="text-truncate">
+                          Account per la pubblicazione di offerte
+                        </Dropdown.Item>
                         <hr className="m-0" />
                         <Dropdown.Item>Esci</Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
                   </Col>
-                  <Col className="py-1 d-none d-sm-block" style={{ height: "100%" }}>
+                  <Col
+                    className="py-1 d-none d-sm-block"
+                    style={{ height: "100%" }}
+                  >
                     <Link
                       onClick={handleToggleOffcanvas}
                       className="nav-link d-flex flex-column p-0 align-items-center my-navbar-text justify-content-center "
@@ -323,9 +393,16 @@ const MyNavbar = () => {
                         Per le aziende
                       </Dropdown.Toggle>
                     </Link>
-                    <MyNavbarOffcanvas show={showOffcanvas} hide={handleToggleOffcanvas} />
+                    <MyNavbarOffcanvas
+                      show={showOffcanvas}
+                      hide={handleToggleOffcanvas}
+                    />
                   </Col>
-                  <Col xs={3} className=" d-none d-md-block" style={{ height: "100%" }}>
+                  <Col
+                    xs={3}
+                    className=" d-none d-md-block"
+                    style={{ height: "100%" }}
+                  >
                     <Link
                       to="/premium/"
                       className=" d-flex flex-column p-0 align-items-center my-navbar-text justify-content-center"
