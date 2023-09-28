98d093e6570ab712a3f93969efd5b6a8269a30f3
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-28 11:55:54 +0200

    now you can modify the profile image

--- src/components/Profile/HeroSection/UserBanner.jsx
+++ src/components/Profile/HeroSection/UserBanner.jsx
@@ -1,119 +1,92 @@
-import { Image } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { Button, Form, Image, Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { modifyImageProfileAction } from "../../../redux/actions";
 
 const UserBanner = () => {
+  const myProfile = useSelector((state) => state.profiles.myProfile);
   const currentProfile = useSelector((state) => state.profiles.currentProfile);
+  const dispatch = useDispatch();
   const params = useParams();
-  // const [show, setShow] = useState(false);
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [formData, setFormData] = useState(new FormData());
+
+  const onFileChange = (e) => {
+    if (e.target && e.target.files[0]) {
+      formData.append("profile", e.target.files[0]);
+      setFormData(formData);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(modifyImageProfileAction(myProfile._id, formData));
+  };
+
   return (
     <div id="user-banner">
       {!params.id && <i className="bi bi-camera-fill pp"></i>}
       {!params.id ? (
         <div className="img-wrapper">
-          <Image src={currentProfile.image} alt="img-profile" fluid />
-          {/* <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modifica l'immagine del profilo</Modal.Title>
-        </Modal.Header>
-        <Modal.Body
-          style={{
-            padding: "8px 24px 16px",
-            maxHeight: "725px",
-            overflowY: "scroll",
-          }}
-        >
-          <p style={{ fontSize: "12px", margin: "0" }}>
-            * Indica che è obbligatorio
-          </p>
-          <Form className="pt-4" onSubmit={handleSubmit}>
-            <div className="pb-4">
-              <Form.Label className="my-label">Nome*</Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci il tuo nome"
-                aria-describedby="basic-addon3"
-                value={query.name}
-                onChange={(e) => setQuery({ ...query, name: e.target.value })}
-                required
-              />
-            </div>
-            <div className="pb-4">
-              <Form.Label className="my-label">Cognome*</Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci il tuo cognome"
-                aria-describedby="basic-addon3"
-                value={query.surname}
-                onChange={(e) =>
-                  setQuery({ ...query, surname: e.target.value })
-                }
-                required
-              />
-            </div>
-            <div className="pb-4">
-              <Form.Label className="my-label">Username*</Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci il tuo nome"
-                aria-describedby="basic-addon3"
-                value={query.username}
-                onChange={(e) =>
-                  setQuery({ ...query, username: e.target.value })
-                }
-                required
-              />
-            </div>
-            <div className="pb-4">
-              <Form.Label className="my-label">Bio</Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci la tua bio"
-                aria-describedby="basic-addon3"
-                value={query.bio}
-                onChange={(e) => setQuery({ ...query, bio: e.target.value })}
-              />
-            </div>
-            <h5 className="m-0 pb-1">Posizione attuale</h5>
-            <div className="pb-4">
-              <Form.Label className="my-label">
-                Posizione lavorativa*
-              </Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci la tua posizione lavorativa"
-                aria-describedby="basic-addon3"
-                value={query.title}
-                onChange={(e) => setQuery({ ...query, title: e.target.value })}
-                required
-              />
-            </div>
-            <h5 className="m-0 pb-1">Località</h5>
-            <div className="pb-1">
-              <Form.Label className="my-label">
-                Paese/Area geografica*
-              </Form.Label>
-              <Form.Control
-                className="my-input-control"
-                placeholder="Inserisci il tuo paese"
-                aria-describedby="basic-addon3"
-                value={query.area}
-                onChange={(e) => setQuery({ ...query, area: e.target.value })}
-                required
-              />
-            </div>
-            <Button variant="primary" type="submit" onClick={handleClose}>
-              Salva
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal> */}
+          <Image
+            src={currentProfile.image}
+            alt="img-profile"
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              height: "100%",
+            }}
+            onClick={handleShow}
+          />
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modifica l'immagine del profilo</Modal.Title>
+            </Modal.Header>
+            <Modal.Body
+              style={{
+                padding: "8px 24px 16px",
+                maxHeight: "725px",
+                overflowY: "scroll",
+              }}
+            >
+              <p style={{ fontSize: "12px", margin: "0" }}>
+                * Indica che è obbligatorio
+              </p>
+              <Form className="pt-4" onSubmit={handleSubmit}>
+                <div className="pb-4">
+                  <Form.Label className="my-label">
+                    Immagine profilo*
+                  </Form.Label>
+                  <Form.Group controlId="formFile" className="mb-3">
+                    <Form.Label>Scegli la tua immagine profilo</Form.Label>
+                    <Form.Control
+                      type="file"
+                      onChange={(e) => onFileChange(e)}
+                    />
+                  </Form.Group>
+                </div>
+                <Button variant="primary" type="submit" onClick={handleClose}>
+                  Salva
+                </Button>
+              </Form>
+            </Modal.Body>
+          </Modal>
         </div>
       ) : (
         <div className="img-wrapper">
-          <Image src={currentProfile.image} alt="img-profile" fluid />
+          <Image
+            src={currentProfile.image}
+            alt="img-profile"
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              height: "100%",
+            }}
+          />
         </div>
       )}
     </div>
