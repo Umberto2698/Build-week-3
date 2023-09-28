a83b1df1b134a2bc66599e8cd821dec28fc4bc2d
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-09-28 16:11:52 +0200

    style adjustments

--- src/components/Home/HomeLeftSidebar/HomeLeftSidebarContainer.jsx
+++ src/components/Home/HomeLeftSidebar/HomeLeftSidebarContainer.jsx
@@ -1,12 +1,10 @@
 import { Col } from "react-bootstrap";
 import HomeLeftSidebarTop from "./HomeLeftSidebarTop";
-import HomeLeftSidebarBottom from "./HomeLeftSidebarBottom";
 
 const HomeLeftSidebarContainer = () => {
   return (
     <Col xs={12} md={3}>
       <HomeLeftSidebarTop />
-      <HomeLeftSidebarBottom />
     </Col>
   );
 };
